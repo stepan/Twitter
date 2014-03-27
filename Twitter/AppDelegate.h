d036509b5aaa21ca2d9bc75e65520362c321ff6d
Stepan Grigoryan <stepang@gmail.com>
2014-03-27 16:05:28 -0700

    Initial commit

---
+++ Twitter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Twitter
+//
+//  Created by Stepan Grigoryan on 3/27/14.
+//  Copyright (c) 2014 Stepan Grigoryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Twitter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Twitter
+//
+//  Created by Stepan Grigoryan on 3/27/14.
+//  Copyright (c) 2014 Stepan Grigoryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
